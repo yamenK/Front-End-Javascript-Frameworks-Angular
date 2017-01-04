4903dfa87b5324f4400032c168936b9d6869db1a
ykassamany <dev.yamenk@gmail.com>
2017-01-04 18:36:18 +0200

    Initialized a dish object and displayed it using a media item

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+    dish=
+        {
+            name: 'Uthapizza',
+            image: '../../assets/images/uthapizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comment: ''
+        }
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+    }
 }
