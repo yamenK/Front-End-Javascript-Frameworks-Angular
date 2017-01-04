3f3e862b49d40edc1ff9e22f8f950b6b163e124d
ykassamany <dev.yamenk@gmail.com>
2017-01-04 18:57:44 +0200

    Exercise : Introduction to AngularJS Object Array and ngFor directive

--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -6,16 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-    dish=
+    dishes=[
         {
-            name: 'Uthapizza',
+            name:'Uthapizza',
             image: '../../assets/images/uthapizza.png',
             category: 'mains',
-            label: 'Hot',
-            price: '4.99',
-            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            label:'Hot',
+            price:'4.99',
+            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comment: ''
+        },
+        {
+            name:'Zucchipakoda',
+            image: '../../assets/images/zucchipakoda.png',
+            category: 'appetizer',
+            label:'',
+            price:'1.99',
+            description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+            comment: ''
+        },
+        {
+            name:'Vadonut',
+            image: '../../assets/images/vadonut.png',
+            category: 'appetizer',
+            label:'New',
+            price:'1.99',
+            description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+            comment: ''
+        },
+        {
+            name:'ElaiCheese Cake',
+            image: '../../assets/images/elaicheesecake.png',
+            category: 'dessert',
+            label:'',
+            price:'2.99',
+            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
             comment: ''
         }
+    ]
 
     constructor() { }
 
