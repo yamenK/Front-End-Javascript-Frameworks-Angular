3f1188072366d59e85597b8b499eb9e7ce2971ec
ykassamany <dev.yamenk@gmail.com>
2017-01-05 19:53:22 +0200

    Exercise Angular Filters Partial implementation for form validation part

--- conFusion/src/app/contactus/contactus.component.ts
+++ conFusion/src/app/contactus/contactus.component.ts
@@ -9,9 +9,33 @@ export class ContactusComponent implements OnInit {
   feedback =  {mychannel:"", firstName:"", lastName:"",
     agree: false, email:"", tel:{number:"", areaCode:""}, comments: ""};
 
-  constructor() { }
+  channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
+
+
+    constructor() {
+        // this.feebackForm
+    }
+  
+  invalidChannelSelection : boolean = false;
 
   ngOnInit() {
   }
+
+    sendFeedback() {
+        console.log(this.feedback);
+
+        if (this.feedback.agree && (this.feedback.mychannel == "")) {
+            this.invalidChannelSelection = true;
+            console.log('incorrect');
+        }
+        else {
+            this.invalidChannelSelection = false;
+            this.feedback =  {mychannel:"", firstName:"", lastName:"",
+                agree: false, email:"", tel:{number:"", areaCode:""}, comments: ""};
+            this.feedback.mychannel = "";
+            // this.feedbackForm.$setPristine();
+            console.log(this.feedback);
+        }
+    };
 
 }
