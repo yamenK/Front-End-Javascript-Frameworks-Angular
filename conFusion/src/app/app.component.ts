b32d96cf8c0a9e782a25e736056f5cbb3ae564e7
yamenK <yamen.kass@gmail.com>
2017-01-07 22:30:30 +0200

    Exercise: Angular Templates

--- conFusion/src/app/app.component.ts
+++ conFusion/src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-menu></app-menu>
-`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
